318c75d49c8cf4b4fabea7f4528373b5ba3ce02e
INTELLIAI <137497099+INTELLIAI@users.noreply.github.com>
2023-06-25 16:22:03 +0000

    basic intro

---
+++ Intro/00_basic.py
@@ -0,0 +1,7 @@
+# python is general purpose preogramming language.
+# Easiest language
+# Reddit is code in python
+# Guido van Rossum build python named after a comedy show.
+# Machine learning and Artifical intelligence
+# It is Interpreterd language we need interpreter.
+# IDE Intergrated Development Enviorment is used to write code.
